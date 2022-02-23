361e70e4c4d77293ad325d5fd1073bb2e394ad86
Shoubhit Dash <shoubhit2005@gmail.com>
2022-02-23 15:16:23 +0530

    changed some important stuff

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
+import Head from 'next/head'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 import About from '../components/About'
@@ -14,20 +15,33 @@ export default function Home() {
     })
   })
   return (
-    <div className="min-h-screen bg-gray-100 py-10 px-3 sm:px-5">
-      <div data-aos="fade-down" data-aos-duration="800">
-        <Card />
+    <div>
+      <Head>
+        <title>Shoubhit Dash | Home</title>
+        <link rel="icon" href="/favicon/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="Shoubhit Dash aka nexxel's personal website."
+        />
+        {/* TODO: Add image */}
+        <meta name="og:image" content="http://ia.media-imdb.com/rock.jpg" />
+      </Head>
+      <div className="min-h-screen bg-gray-100 py-10 px-3 sm:px-5">
+        <div data-aos="fade-down" data-aos-duration="800">
+          <Card />
+        </div>
+        <div data-aos="fade-down" data-aos-duration="800" data-aos-delay="400">
+          <About />
+        </div>
+        <div data-aos="fade-down" data-aos-duration="800" data-aos-delay="400">
+          <Skills />
+        </div>
+        <div data-aos="fade-down" data-aos-duration="800" data-aos-delay="400">
+          <Contact />
+        </div>
+        <Footer />
       </div>
-      <div data-aos="fade-down" data-aos-duration="800" data-aos-delay="400">
-        <About />
-      </div>
-      <div data-aos="fade-down" data-aos-duration="800" data-aos-delay="400">
-        <Skills />
-      </div>
-      <div data-aos="fade-down" data-aos-duration="800" data-aos-delay="400">
-        <Contact />
-      </div>
-      <Footer />
     </div>
   )
 }
