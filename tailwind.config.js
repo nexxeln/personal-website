3d0a08564fa506585da9e0a4760e3ea56613cb89
Shoubhit Dash <shoubhit2005@gmail.com>
2022-02-23 15:09:18 +0530

    extended fontFamily to make a class for manrope

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
