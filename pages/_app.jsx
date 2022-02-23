a7102d04970ffeef763bc42c1e3159cec14cdf70
Shoubhit Dash <shoubhit2005@gmail.com>
2022-02-23 15:17:06 +0530

    added nprogress stuff

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
-import { AppProps } from 'next/app'
+import '../styles/nprogress.css'
+import Router from 'next/router'
+import NProgress from 'nprogress'
 
 function MyApp({ Component, pageProps }) {
+  NProgress.configure({ showSpinner: false })
+  Router.events.on('routeChangeStart', () => NProgress.start())
+  Router.events.on('routeChangeComplete', () => NProgress.done())
+  Router.events.on('routeChangeError', () => NProgress.done())
+
   return <Component {...pageProps} />
 }
 
