f28b0dac16cf36b9b0e6da5b385e67827212352c
Shoubhit Dash <shoubhit2005@gmail.com>
2022-02-23 15:13:34 +0530

    made the button nicer

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -4,14 +4,14 @@ import { FaArrowRight } from 'react-icons/fa'
 function Contact() {
   return (
     <div
-      className="m-auto w-full pt-36 pb-10 text-center font-mono"
+      className="m-auto w-full pt-16 pb-16 text-center font-mono"
       id="contactme"
     >
       <h1 className="text-4xl">want to work together?</h1>
       <a href="mailto:shoubhit2005@gmail.com">
-        <span className="mt-6 inline-block w-auto cursor-pointer bg-black py-4 px-7 font-mono text-base text-white md:text-xl">
+        <span className="mt-6 inline-block w-auto cursor-pointer bg-black py-4 px-7 font-mono text-base text-white opacity-90 shadow-lg hover:rounded-sm hover:opacity-100 hover:shadow-2xl md:text-xl">
           <div className="flex flex-row items-center">
-            <span className="mr-3 ">Get in touch</span>
+            <span className="mr-3">get in touch</span>
             <FaArrowRight color="white" />
           </div>
         </span>
