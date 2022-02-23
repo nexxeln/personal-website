be97b7f539e7d3f5dc4163c520d964b46424ace7
Shoubhit Dash <shoubhit2005@gmail.com>
2022-02-23 15:12:21 +0530

    updated stuff and included link for 'code_art'

--- components/About.jsx
+++ components/About.jsx
@@ -1,13 +1,21 @@
+import Link from 'next/link'
 import React from 'react'
 
 function About() {
   return (
-    <div className="mx-auto max-w-4xl px-4 pt-12">
+    <div className="mx-auto max-w-4xl px-4 pt-8">
       <p className="text-left font-mono text-2xl text-black md:text-4xl">hi</p>
       <p className="mt-4 text-left font-mono text-base leading-relaxed text-gray-600 sm:text-xl">
-        i'm a student currently in 12th grade. i'm aspiring to get my bachelors
+        i'm a student currently in 11th grade. i'm aspiring to get my bachelors
         degree in computer science. i'm a self-taught developer and am a huge
-        believer in open-source and open-access.
+        believer in open-source and open-access. i also like making{' '}
+        <Link href="/code_art" passHref>
+          <a>
+            <span className="cursor-pointer bg-gradient-to-r from-orange-600 to-blue-600 bg-clip-text  text-transparent  opacity-80 transition-opacity duration-500 hover:bg-gradient-to-l hover:opacity-100">
+              art with code.
+            </span>
+          </a>
+        </Link>
       </p>
     </div>
   )
