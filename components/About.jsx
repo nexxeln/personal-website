b5b5dc0f9a0d11b4a1ea451dae4b2b6b3a103c21
Shoubhit Dash <shoubhit2005@gmail.com>
2022-04-08 10:30:57 +0530

    update bio

--- components/About.jsx
+++ components/About.jsx
@@ -7,8 +7,7 @@ function About() {
       <p className="font-mono text-2xl text-left text-black md:text-4xl">hi</p>
       <p className="mt-4 font-mono text-base leading-relaxed text-left text-gray-600 sm:text-xl">
         i'm a student currently in 12th grade. i'm aspiring to get my bachelors
-        degree in computer science. i'm a self-taught developer and am a huge
-        believer in open-source and open-access. i also like making{' '}
+        degree in computer science. i'm a self-taught developer. i also like making{' '}
         <Link href="/code_art" passHref>
           <a>
             <span className="text-transparent transition-opacity duration-500 cursor-pointer bg-gradient-to-r from-orange-600 to-blue-600 bg-clip-text opacity-80 hover:bg-gradient-to-l hover:opacity-100">
